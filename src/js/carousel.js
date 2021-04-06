072e211d6958ce2417927425a4b160610d3ee58b
Demilicz <Necrolick92@gmail.com>
2021-04-06 19:48:50 +0200

    full adaptive & full landing page

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -6,14 +6,29 @@ $(function() {
     draggable: false,
     slidesToShow: 3,
     slidesToScroll: 2,
-    // responsive: [
-    //   {
-    //     breakpoint: 992,
-    //     settings: {
-    //       arrows: false,
-    //       draggable: true
-    //     }
-    //   }
-    // ]
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          arrows: false,
+          draggable: true,
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
 });
